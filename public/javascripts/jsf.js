58cc39226537303614005af0b1ea8f81af645f8e
Tamrei <Tamreix@gmail.com>
2016-04-28 18:00:52 +0300

    iframe3

--- public/javascripts/jsf.js
+++ public/javascripts/jsf.js
@@ -1,70 +1,100 @@
-/* From in side the script.... */
-
-// Reliably grab my script tag
 'use strict';
 
 var script = document.getElementsByTagName("script");
 script = script[script.length - 1];
 
 var scriptAtributes = script.attributes;
-var ifrm = document.createElement("IFRAME");
+var ifrm = document.createElement("iframe");
 ifrm.frameBorder = 0;
 ifrm.id = "thisframe";
 
-//ifrm.src = "/javascripts/iframeTestSrc2.html";
 prepareFrame(ifrm, script.parentElement);
-//ifrm.getElementsByTagName("html")[0].setAttribute("id", "doc");
 
 var iframeDoc = ifrm.contentWindow.document;
 
 var html =
         '<div id="checkout_wrapper">' +
         '<div id="doc">' +
-        '<form>' +
-        '<label for="order_id">Order_id</label>' +
-        '<input type="text" id="order_id" name="order_id" value="test4207135583456">' +
-        '<label for="order_desc">Order_desc</label>' +
+        '<form id="phone_form">' +
+        //'<label for="order_id">Order_id</label>' +
+        //'<input type="text" id="order_id" name="order_id" value="test4207135583456">' +
+        '<label for="phone_number">Phone number</label>' +
+        '<input type="text" onchange="validate()" id="phone_number" name="phone_number" placeholder="phone_number">' +
+        '<label for="order_desc">Order desc</label>' +
         '<input type="text" id="order_desc" name="order_desc" value="Test payment">' +
         '<label for="amount">Amount</label>' +
         '<input type="text" id="amount" name="amount" value="100">' +
         '<label for="currency">Currency</label>' +
         '<input type="text" id="currency" name="currency" value="RUB">' +
-        '<label for="merchant_id">merchant_id (use 1396424 for test)</label>' +
+        '<label for="merchant_id">merchant_id (use 1396424 for testing)</label>' +
         '<input type="text" id="merchant_id" name="merchant_id" value="1396424">' +
         '</form>' +
         '<button onclick="sendData()">Proceed Payment</button>' +
-        //'<button onclick="hideIndex()">Hide Index</button>' +
-        //'<button onclick="iframeProceed()">Send data</button>' +
         '</div>' +
         '</div>';
-
 
 var initialCss =
     "input[type=text] {" +
     "width: 100%;" +
     "padding: 12px 20px;" +
     "margin: 8px 0;" +
-    "box-sizing: border-box;}";
+    "box-sizing: border-box;}" +
+    "button {" +
+    "background-color: #4CAF50;" +
+    "border: none;" +
+    "color: white;" +
+    "padding: 15px 22px;" +
+    "text-align: center;" +
+    "text-decoration: none;" +
+    "display: inline-block;" +
+    "font-size: 15px;" +
+    "margin: 2px 0 2px 0;" +
+    "cursor: pointer;}";
 
-var css = "h1 { color: white }";/* +
-    "'html , body' : {" +
-"'overflow' : 'hidden'}";*/
+var css = "h1 { color: white }";
 
-var iframeScript = 'function sendData() {' +
-    'var orderId = document.getElementById("order_id");' +
+
+var phoneValidateScript =
+        'function validate() {' +
+        'var phone_number = document.getElementById("phone_number");' +
+        'var phone_form = document.getElementById("phone_form");' +
+        'var data = { phone_number: phone_number.value };' +
+        'var xmlhttp = new XMLHttpRequest();' +
+        'var url = "/payment/validatePhone";' +
+        'xmlhttp.open("post", url, true);' +
+        'xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");' +
+        'xmlhttp.onreadystatechange = function () {' +
+        'if (xmlhttp.readyState == XMLHttpRequest.DONE) {' +
+        'if (xmlhttp.status == 200) {' +
+        'if (xmlhttp.response == "error") {' +
+        'console.log("error");' +
+        '} else if (xmlhttp.response == "success") {' +
+        'console.log("success");' +
+        '} else {' +
+        'console.log(xmlhttp.status);' +
+        '}' +
+        '}' +
+        '};' +
+        'xmlhttp.send(JSON.stringify(data));' +
+        '};';
+
+
+var iframeScript =
+    'function sendData() {' +
+    //'var orderId = document.getElementById("order_id");' +
     'var orderDesc = document.getElementById("order_desc");' +
     'var currency = document.getElementById("currency");' +
     ' var amount = document.getElementById("amount");' +
     'var merchantID = document.getElementById("merchant_id");' +
     'var data = {' +
-    '"order_id": orderId.value,' +
+    //'"order_id": orderId.value,' +
     '"order_desc": orderDesc.value,' +
     '"currency": currency.value,' +
     '"amount": amount.value,' +
     '"merchant_id": merchantID.value' +
     '};' +
     'var xmlhttp = new XMLHttpRequest();' +
-    'var url = "/getPaymentForm";' +
+    'var url = "/payment/getPaymentForm";' +
     'xmlhttp.open("post", url, true);' +
     'xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");' +
     'xmlhttp.onreadystatechange = function () {' +
@@ -83,72 +113,62 @@ var iframeScript = 'function sendData() {' +
     'xmlhttp.send(JSON.stringify(data));' +
     '};';
 
-var iframeHideIndex = 'function hideIndex() {' +
+var iframeHideIndex =
+        'function hideIndex() {' +
         'document.getElementById("doc").style.display = "none";' +
         'document.getElementsByTagName("body")[0].style.margin = "0";' +
         'iframeProceed();' +
-        '' +
-        '' +
         '}';
 
-var iframeProceed = 'function iframeProceed(url) {' +
+var iframeProceed =
+        'function iframeProceed(url) {' +
         '' +
         '$ipsp("checkout").scope(function() {' +
         'this.setCheckoutWrapper("#checkout_wrapper");' +
         'this.addCallback(__DEFAULTCALLBACK__);' +
         'this.loadUrl(url);' +
         '});' +
-        //'$ipsp("checkout").loadUrl("https://api.fondy.eu/api/checkout?token=c298b5074bac6707cbb2d58ab1716b5615e44de3")' +
         '};';
 
-var iframeDefaultCallback = 'function __DEFAULTCALLBACK__(data,type){' +
+var iframeDefaultCallback =
+        'function __DEFAULTCALLBACK__(data,type){' +
         'var form;' +
         'if ( data.error){' +
         'return;' +
-    '}' +
-    'if (data.action == "redirect") {' +
-    'this.loadUrl(data.url);' +
-    'return;' +
-    '}' +
-    'if (data.send_data.order_status == "delayed") {' +
-    'this.unbind("ready");' +
-    'this.hide();' +
-    'return;' +
-    '} else {' +
-    'this.unbind("ready").action("ready", function() {' +
-    'this.show();' +
-    '});' +
-    '}' +
-    'if( data.send_data && data.url ){' +
-    'form = prepareFormData(data.url,data.send_data);' +
-    'this.find("body").appendChild(form);' +
-    'form.submit();' +
-    'form.parentNode.removeChild(form);';
+        '}' +
+        'if (data.action == "redirect") {' +
+        'this.loadUrl(data.url);' +
+        'return;' +
+        '}' +
+        'if (data.send_data.order_status == "delayed") {' +
+        'this.unbind("ready");' +
+        'this.hide();' +
+        'return;' +
+        '} else {' +
+        'this.unbind("ready").action("ready", function() {' +
+        'this.show();' +
+        '});' +
+        '}' +
+        'if( data.send_data && data.url ){' +
+        'form = prepareFormData(data.url,data.send_data);' +
+        'this.find("body").appendChild(form);' +
+        'form.submit();' +
+        'form.parentNode.removeChild(form);';
 
 
-//prepareScriptTag("https://api.fondy.eu/static_common/v1/checkout/ipsp.js");
 prepareScriptTag("/javascripts/ipsp.js");
-prepareScriptTag("/javascripts/testFile.js");
 prepareHTML(html);
-
-//addToHead();
 prepareStyle(css);
 prepareStyle(initialCss);
+prepareScript(phoneValidateScript);
 prepareScript(iframeDefaultCallback);
 prepareScript(iframeScript);
 prepareScript(iframeProceed);
 prepareScript(iframeHideIndex);
 
 
-if (script.hasAttribute('data-color')) {
-}
-//<script src="/javascripts/ipsp.js"></script>
-
-        function addToHead() {
-            var ifrmHead = iframeDoc.getElementsByTagName('head')[0];
-            ifrmHead.innerHTML += '<script src="/javascripts/testFile.js"></script>';
-            ifrmHead.innerHTML += '<script src="/javascripts/ipsp.js"></script>';
-        }
+//if (script.hasAttribute('data-color')) {
+//}
 
 function prepareStyle(css) {
     var ifrmHead = iframeDoc.getElementsByTagName('head')[0];
@@ -158,27 +178,26 @@ function prepareStyle(css) {
 }
 
 function prepareScriptTag(src) {
-    //var myIframe = document.getElementById("myIframeId");
-
     var ifrmdoc = ifrm.contentWindow.document;
-    //var script = ifrm.contentWindow.document.createElement("script");
-    var script2 = ifrmdoc.getElementsByTagName('head')[0];//.createElement("script");
-
+    var script2 = ifrmdoc.getElementsByTagName('head')[0];
     var script = iframeDoc.createElement("script");
-
     script.type = "text/javascript";
     script.src = src;
     script2.appendChild(script);
-    //ifrm.contentWindow.document.body.appendChild(script);
 }
 
 function prepareFrame(ifrm, container) {
-    //var ifrm = document.createElement("IFRAME");
-    //ifrm.setAttribute("src", "http://google.com/");
-    ifrm.style.width = 600 + "px";
-    ifrm.style.height = 480 + "px";
-    ifrm.style.backgroundColor = script.getAttribute("data-background-color");
+    setStyles();
     container.appendChild(ifrm);
+}
+
+function setStyles() {
+    ifrm.style.width = script.getAttribute("data-width") + "px";
+    ifrm.style.height = script.getAttribute("data-height") + "px";
+    ifrm.style.backgroundColor = script.getAttribute("data-background-color");
+    ifrm.style.color = script.getAttribute("data-color");
+    ifrm.style.border = script.getAttribute("data-body-border");
+    ifrm.style.borderRadius = script.getAttribute("data-body-border-radius") + "px";
 }
 
 function prepareHTML(html) {
@@ -193,39 +212,3 @@ function prepareScript (s) {
     script.innerHTML = s;
     ifrmHead.appendChild(script);
 }
-
-
-
-
-
-/*
- // Create a random seed value, making it almost impossible to
- // determine what is being tested for.
- var prevent_bust = Math.random() * 3000;
-
- // enclose everything in a function, so that it cannot be addressed
- function iniFunc ( init ) {
- // The function is no longer in scope of the main window.
- function onbeforeunload() { prevent_bust++ }
- window.onbeforeunload = onbeforeunload;
- setInterval( function() {
- // make sure the function was not deleted.
- if( window.onbeforeunload != onbeforeunload )
- {
- prevent_bust = init + 1;
- window.onbeforeunload = onbeforeunload;
- }
- if (prevent_bust > init ) {  // All comparison is to the random seed.
- prevent_bust -= 2;
- //window.top.location = '/iframe';
- // Unfortunately, you have absolutely no idea which website caused
- // the incrementation, so you cannot replace it with a link!
- //
- // You might try to simply ignore it and just use the iframe as is --
- // theoretically, they are no longer able to bust this frame.
- // (this theory will be disproved below).
- }
- }, 1 );
- };
- iniFunc( prevent_bust );
- */
---
+++ routes/payment.js
@@ -0,0 +1,110 @@
+var express = require('express');
+var router = express.Router();
+
+var qs = require("qs");
+
+var sha1 = require('sha1');
+var sha256 = require('sha256');
+
+var request = require('request');
+
+var uuid = require('uuid');
+
+
+function sortObject(o) {
+    var sorted = {},
+        key, a = [];
+
+    for (key in o) {
+        if (o.hasOwnProperty(key)) {
+            a.push(key);
+        }
+    }
+
+    a.sort();
+
+    for (key = 0; key < a.length; key++) {
+        sorted[a[key]] = o[a[key]];
+    }
+    return sorted;
+}
+
+
+function getSignaturizableStr(obj) {
+    var result = "";
+
+    var pass = "test";
+
+    result += pass;
+
+    for (var o in obj) {
+        result += "|" + obj[o];
+    }
+    return result;
+}
+
+
+router.post('/getPaymentForm', function (req, res, next) {
+    var data = req.body;
+    var generatedOrderId = uuid.v1();
+    data.order_id = generatedOrderId;
+
+    data = JSON.stringify(data);
+    data = JSON.parse(data);
+    data = sortObject(data);
+
+    data.signature = sha1(getSignaturizableStr(data));
+
+    var stq = qs.stringify(data);
+
+    request({
+        url: 'https://api.fondy.eu/api/checkout/url/', method: "POST",
+        headers: {"content-type": "application/x-www-form-urlencoded"}, body: stq
+    }, function (error, response, body) {
+        var resObj = (qs.parse(body));
+        if (resObj.response_status == "success") {
+            console.log(resObj);
+            res.send(resObj);
+        } else {
+            console.log(resObj);
+            res.status(500).end();
+        }
+    });
+});
+
+
+router.post('/validatePhone', function (req, res, next) {
+    var data = req.body;
+    data.payment_processor_id = "1j1da142-d95c-7c27-5aff-a61dcc80c2b2";
+    var pass = "9295d3b808bcd8d720";
+
+    var str = pass + "|" + data.payment_processor_id + "|" + data.phone_number;
+
+    var obj = {
+        "phone_number": data.phone_number,
+        "payment_processor_id": data.payment_processor_id,
+        "signature": sha256(str)
+    };
+
+    request({
+        url: 'http://192.168.150.162:9004/can-topup', method: "POST",
+        headers: {"content-type": "application/json"}, body: JSON.stringify(obj)
+    }, function (error, response, body) {
+
+        if (error) {
+            res.status(500).end();
+        }
+
+        var resp = JSON.parse(body);
+        console.log(resp);
+
+        if (resp.can_topup_account == true) {
+            res.send("success");
+        } else {
+            res.send("error");
+        }
+    });
+});
+
+
+module.exports = router;
